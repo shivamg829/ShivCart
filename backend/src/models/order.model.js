f4382cef6a370c1cf6d30d0fbf19f4057a58a19b
shivamg829 <shivamgangwar2197@gmail.com>
2026-08-18 21:45:14 +0530

    feat: add order model

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,162 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    orderItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        productName: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+        image: {
+          type: String,
+          default: "",
+        },
+      },
+    ],
+
+    shippingAddress: {
+      houseNo: {
+        type: String,
+        required: true,
+      },
+      street: {
+        type: String,
+        required: true,
+      },
+      landmark: {
+        type: String,
+        default: "",
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: String,
+        required: true,
+      },
+    },
+
+    paymentMethod: {
+      type: String,
+      enum: ["COD", "ONLINE"],
+      required: true,
+    },
+
+    paymentResult: {
+      id: {
+        type: String,
+        default: "",
+      },
+      status: {
+        type: String,
+        default: "",
+      },
+      update_time: {
+        type: String,
+        default: "",
+      },
+      email_address: {
+        type: String,
+        default: "",
+      },
+    },
+
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0,
+    },
+
+    orderStatus: {
+      type: String,
+      enum: [
+        "Pending",
+        "Confirmed",
+        "Processing",
+        "Shipped",
+        "Out for Delivery",
+        "Delivered",
+        "Cancelled",
+      ],
+      default: "Pending",
+    },
+
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+
+    paidAt: {
+      type: Date,
+    },
+
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
